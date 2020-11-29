e14c0ccc86df02a03f6ddb50ba73e28ef007bfea
Admar Schoonen <admar@familieschoonen.nl>
2020-11-29 13:04:21 +0100

    Added opentherm gateway interface

---
+++ otgw/otgw.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+import asyncio
+import json
+import time
+from pyotgw import pyotgw
+import sys
+import socket
+import requests
+
+global got_socket
+global s
+global metrics
+global paths
+
+got_socket = False
+s = None
+
+with open('/etc/energy-monitoring-with-graphite/otgw/settings.json') as json_data:
+    d = json.load(json_data)
+    device = d['device']
+    metrics = d['metrics']
+    paths = d['paths']
+    interval = d['interval']
+    server = d['server']
+    port = d['port']
+
+t_now = time.time()
+s = None
+
+async def print_status(status):
+  """Receive and print status."""
+  global got_socket
+  global metrics
+  global paths
+  global s
+
+  print("Received a status update:\n{}".format(status))
+  t_start = time.time()
+  #print(str(telegram[obis_references.P1_MESSAGE_TIMESTAMP].value))
+  t = ''
+  n = -1
+  for m in metrics:
+      x = m.split('.')
+      base = x[0]
+      ext = x[1]
+      p = ''
+      n = n + 1
+      try:
+          V = json.loads("{}".format(status).replace('\'', '"').replace('None', '"None"'))
+          v = str(float(V[base][ext]))
+          p = paths[n]
+      except:
+          continue
+          
+      if p != '':
+          t = t + p + ' ' + v + ' ' + str(int(t_start)) + '\n'
+  
+  if t != '':
+      if got_socket == False:
+          try:
+              s = socket.socket()
+              s.connect((server, port))
+              got_socket = True
+          except:
+              s.close()
+              got_socket = False
+  
+      if got_socket:
+          try:
+              s.send(t.encode('ascii'))
+              print(time.ctime(t_start) + ' otgw: sent ' + t)
+          except:
+              print(time.ctime(t_start) + ' otgw: Error: Could not send message (send failed).')
+              s.close()
+              got_socket = False
+      else:
+          print(time.ctime(t_start) + ' otgw: Error: Could not send message (no socket).')
+
+async def connect_and_subscribe():
+  """Connect to the OpenTherm Gateway and subscribe to status updates."""
+
+  # Create the object
+  gw = pyotgw()
+
+  # Connect to OpenTherm Gateway on device
+  status = await gw.connect(asyncio.get_event_loop(), device)
+  print("Initial status after connecting:\n{}".format(status))
+
+  # Subscribe to updates from the gateway
+  if not gw.subscribe(print_status):
+    print("Could not subscribe to status updates.")
+
+  # Keep the event loop alive...
+  while True:
+    await asyncio.sleep(1)
+
+
+# Set up the event loop and run the connect_and_subscribe coroutine.
+loop = asyncio.get_event_loop()
+try:
+  loop.run_until_complete(connect_and_subscribe())
+except KeyboardInterrupt:
+  print("Exiting")
+
+sys.exit(0)
